d076d101820639f51ae941b62e1054749d1bfc41
testman <testman@kompot.si>
2024-01-02 16:15:51 +0100

    handle multiple players

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
-var is_playing
+@export var amount_of_players = 2
+var current_player_count: int
+var ship_scene = preload("res://scenes/ship/ship.tscn")
 
 func process_hud():
 	var text_1 = 0
@@ -12,39 +14,53 @@ func process_hud():
 	$Hud.set_text_11(text_1)
 	$Hud.set_text_21(text_2)
 
-func process_keys():
-	if Input.is_action_pressed("exit"):
-		get_tree().quit()
+func spawn_players(amount):
+	if amount > $SpawnPositions.get_child_count():
+		print("no")
+		return
+	for i in range(amount):
+		var spawn_position = $SpawnPositions.get_child(i)
+		var player_ship = ship_scene.instantiate()
+		add_child(player_ship)
+		player_ship.player_id = i
+		player_ship.position = spawn_position.position
+		player_ship.rotation = spawn_position.rotation
+		player_ship.spawn()
+		player_ship.death.connect(_on_ship_death)
 
 func new_game():
 	print("new game")
 	$Music/MenuMusic.stop()
-	$Music/GameMusic.play()
+	#$Music/GameMusic.play()
 	$StartTimer.start()
+	current_player_count = amount_of_players
 	
 func restart_game():
 	print("restart")
-	is_playing = false
+	set_process(false)
 	$Music/GameMusic.stop()
-	$Music/MenuMusic.play()
+	#$Music/MenuMusic.play()
 	$Hud.show_game_over()
 
 func _ready():
-	is_playing = false
+	set_process(false)
+
+func _input(event):
+	if event.is_action_pressed("exit"):
+		get_tree().quit()
 
 func _process(delta):
-	if not is_playing:
-		return
-	process_keys()
 	process_hud()
 
 func _on_hud_start_game():
 	new_game()
 
 func _on_ship_death():
-	restart_game()
+	current_player_count -= 1
+	if current_player_count == 0:
+		restart_game()
 
 func _on_start_timer_timeout():
 	print("playing")
-	is_playing = true
-	$Ship.spawn()
+	set_process(true)
+	spawn_players(amount_of_players)
